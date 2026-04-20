d6a425570920ad24ad4f1b41164142859ad69482
Durotaren <188862641+Durotaren@users.noreply.github.com>
2026-04-20 15:22:41 +0300

    Replace background Component with a new one

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import './styles/App.css';
 import MainPage from './components/MainPage';
-import FaultyTerminal from './components/FaultyTerminal';
 import IntroductionModal from './components/IntroductionModal';
+import Silk from './components/Silk';
 
 function App() {
   const [firstTime, setFirstTime] = useState<boolean>(() => {
@@ -22,24 +22,12 @@ function App() {
         className="terminal-container"
         style={{ width: '100%', height: '100svh', position: 'relative' }}
       >
-        <FaultyTerminal
-          scale={1.5}
-          gridMul={[2, 1]}
-          digitSize={1.2}
-          timeScale={0.5}
-          pause={false}
-          scanlineIntensity={0.5}
-          glitchAmount={1}
-          flickerAmount={1}
-          noiseAmp={1}
-          chromaticAberration={0}
-          dither={0}
-          curvature={0.1}
-          tint="#00fbff"
-          mouseReact
-          mouseStrength={0.5}
-          pageLoadAnimation
-          brightness={1}
+        <Silk
+          speed={5}
+          scale={1}
+          color="#00fbff"
+          noiseIntensity={1.5}
+          rotation={0}
         />
       </div>
       <MainPage firstTime={firstTime} setFirstTime={setFirstTime} />
